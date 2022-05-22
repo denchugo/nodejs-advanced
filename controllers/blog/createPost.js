a3deafa746c98327c003a41b8b429917bbda2e62
denchugo <tnkstr@live.jp>
2022-05-22 21:19:44 +0900

    DataSort

--- controllers/blog/createPost.js
+++ controllers/blog/createPost.js
@@ -1,7 +1,6 @@
 const BlogModel = require("../../models/blog");
 
 module.exports = (req, res) => {
-  console.log(req.body);
   BlogModel.create(req.body, (error, savedBlogData) => {
     if (error) {
       res.render("error", { message: "/blog/createのエラー" });
