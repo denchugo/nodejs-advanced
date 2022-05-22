4985bc535d539b60708f7b9b46995d6431530bfe
denchugo <tnkstr@live.jp>
2022-05-22 16:14:26 +0900

    DataSort

--- controllers/blog/allBlogs.js
+++ controllers/blog/allBlogs.js
@@ -2,6 +2,6 @@ const BlogModel = require("../../models/blog");
 
 ////全記事読み込み
 module.exports = async(req, res) => {
-  const allBlogs = await BlogModel.find().sort('date')
+  const allBlogs = await BlogModel.find().sort('-createDate')
   res.render("index", { allBlogs: allBlogs, session: req.session.userId })
 }
